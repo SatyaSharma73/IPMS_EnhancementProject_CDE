2548fdffe4c816661ccd465085145f2450e3a90d
Satya Sharma <satyasharma2964@gmail.com>
2022-07-23 16:04:58 +0530

    Pushing Final Code

---
+++ AuthorizationMicroservice/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user (userid,upassword,uname) values ('satyasharma','satyasharma','satyasharma');
+insert into user (userid,upassword,uname) values ('satya2sharma','satya2sharma','satya2sharma');
